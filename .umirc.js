21b5c740731234170ddffe9a5723912235fca698
hocgin <hocgin@gmail.com>
2020-04-21 16:59:28 +0800

    # UPDATE

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,5 @@
 // ref: https://umijs.org/config/
 export default {
-  treeShaking: true,
   routes: [
     {
       path: '/',
@@ -15,29 +14,7 @@ export default {
     dynamicRoot: true,
   },
   outputPath: './dist',
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: false,
-      title: 'Panda',
-      dll: false,
-
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
-  copy: [{
-    from: 'CNAME',
-    to: 'CNAME',
-    toType: 'file',
-  }],
+  dva: {},
+  antd: {},
+  copy: ['CNAME'],
 };
