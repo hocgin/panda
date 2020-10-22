0d18573c0ce9a6b3491e5473afa0f37b777db04a
hocgin <hocgin@gmail.com>
2020-10-22 23:26:10 +0800

    ✋ Use Gin - Footer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,8 @@
 import styles from './index.less';
 import classnames from 'classnames';
 import { HeartFilled } from '@ant-design/icons';
+import GinFooter from '@hocgin/gin-footer'
+import React from "react";
 
 function BasicLayout(props) {
   window.console.log = (...args) => {
@@ -11,16 +13,8 @@ function BasicLayout(props) {
   };
   return (
     <div className={styles.wrapper}>
-      {/*<div className={styles.top}>Yay! Welcome to umi!</div>*/}
       {props.children}
-      <div className={styles.footer}>
-        <div className={styles.footerInfo}>
-          <HeartFilled className={classnames(styles.heartbeat, styles.heart)} />
-          <a className={styles.me} href="https://hocg.in" target="_blank" rel="noopener noreferrer">HOCGIN</a>
-          &nbsp;
-          <span className={styles.year}>2020<sup>&copy;</sup></span>
-        </div>
-      </div>
+      <GinFooter/>
     </div>
   );
 }
