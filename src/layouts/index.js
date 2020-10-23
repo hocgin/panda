935b5652c45624af3a98a230e3762dbd637908b5
hocgin <hocgin@gmail.com>
2020-10-24 01:19:02 +0800

    ✋ Use Gin - Eggs

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,22 +1,22 @@
 import styles from './index.less';
-import classnames from 'classnames';
-import { HeartFilled } from '@ant-design/icons';
+import GinEggs from '@hocgin/gin-eggs';
 import GinFooter from '@hocgin/gin-footer'
 import React from "react";
 
 function BasicLayout(props) {
-  window.console.log = (...args) => {
-    // ..
-  };
-  window.console.debug = (...args) => {
-    // ..
-  };
-  return (
-    <div className={styles.wrapper}>
-      {props.children}
-      <GinFooter/>
-    </div>
-  );
+    window.console.log = (...args) => {
+        // ..
+    };
+    window.console.debug = (...args) => {
+        // ..
+    };
+    return (
+        <div className={styles.wrapper}>
+            <GinEggs/>
+            {props.children}
+            <GinFooter/>
+        </div>
+    );
 }
 
 export default BasicLayout;
