1f8ed52dd67a8b82cedc5006258c1b80c2134014
hocgin <hocgin@gmail.com>
2020-11-22 21:05:15 +0800

    ✋ 百度统计

--- .umirc.js
+++ .umirc.js
@@ -1,21 +1,24 @@
 // ref: https://umijs.org/config/
 export default {
-  title: '熊猫导航',
-  routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' },
-      ],
+    title: '熊猫导航',
+    routes: [
+        {
+            path: '/',
+            component: '../layouts/index',
+            routes: [
+                {path: '/', component: '../pages/index'},
+            ],
+        },
+    ],
+    exportStatic: {
+        htmlSuffix: true,
+        dynamicRoot: true,
     },
-  ],
-  exportStatic: {
-    htmlSuffix: true,
-    dynamicRoot: true,
-  },
-  outputPath: './dist',
-  dva: {},
-  antd: {},
-  copy: ['CNAME'],
+    outputPath: './dist',
+    dva: {},
+    antd: {},
+    copy: ['CNAME'],
+    analytics: {
+        baidu: '41d7f44870aac3490f3d497fc9af7485',
+    },
 };
