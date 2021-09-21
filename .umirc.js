6c5a1048215f79ef588457660e31f40d923deb3d
hocgin <hocgin@gmail.com>
2021-09-21 22:29:06 +0800

    external

--- .umirc.js
+++ .umirc.js
@@ -25,4 +25,17 @@ export default {
     analytics: {
         baidu: 'ea74cfe1769e440fd33ddda0da97d5ec',
     },
+    // 配置 external
+    externals: {
+        'react': 'window.React',
+        'react-dom': 'window.ReactDOM',
+    },
+    // 引入被 external 库的 scripts 区分 development 和 production，使用不同的产物
+    scripts: process.env.NODE_ENV === 'development' ? [
+        'https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.development.js',
+        'https://gw.alipayobjects.com/os/lib/react-dom/16.8.6/umd/react-dom.development.js',
+    ] : [
+        'https://gw.alipayobjects.com/os/lib/react/16.8.6/umd/react.production.min.js',
+        'https://gw.alipayobjects.com/os/lib/react-dom/16.8.6/umd/react-dom.production.min.js',
+    ],
 };
