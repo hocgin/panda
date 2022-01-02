a287e0cc99c4b2daa1bebbad5eea588001c71a41
hocgin <hocgin@gmail.com>
2022-01-02 19:44:17 +0800

    cdn

---
+++ .umirc.prod.cdn.js
@@ -0,0 +1,7 @@
+import {defineConfig} from 'umi';
+
+export default defineConfig({
+    hash: true,
+    favicon: 'https://hocg.in/favicon.ico',
+    publicPath: `https://cdn.hocgin.top/panda/`,
+});
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,6 @@ import {Eggs as GinEggs, Footer as GinFooter} from '@hocgin/ui';
 import React from "react";
 
 function BasicLayout(props) {
-    // window.console.log = (...args) => {
-    //     // ..
-    // };
-    // window.console.debug = (...args) => {
-    //     // ..
-    // };
     return (
         <div className={styles.wrapper}>
             <GinEggs/>
