087f7160ee74f8fec04a4c87f12df7516dd8da9c
hocgin <hocgin@gmail.com>
2022-12-08 20:52:14 +0800

    update

--- .umirc.prod.cdn.js
+++ .umirc.prod.cdn.js
@@ -2,6 +2,6 @@ import {defineConfig} from 'umi';
 
 export default defineConfig({
     hash: true,
-    favicon: 'https://hocg.in/favicon.ico',
+    favicon: 'https://cdn.hocgin.top/favicon.ico',
     publicPath: `https://cdn.hocgin.top/panda/`,
 });
